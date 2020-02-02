0613651ded1cf39f8e801439717653024283134f
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-02-02 18:22:53 +0100

    Change price calculation algorithm

---
+++ src/main/kotlin/domain/NumberExtensions.kt
@@ -0,0 +1,7 @@
+package domain
+
+inline val Int.isEven
+    get() = this % 2 == 0
+
+inline val Int.isOdd
+    get() = this % 2 == 1
--- src/main/kotlin/domain/OrderPriceCalculation.kt
+++ src/main/kotlin/domain/OrderPriceCalculation.kt
@@ -2,27 +2,29 @@ package domain
 
 import java.time.DayOfWeek
 
-const val premiumTicketFee = 3.0
-const val premiumTicketFeeForStudents = 2.0
+const val premiumTicketFee = 3
+const val premiumTicketFeeForStudents = 2
 const val minimumAmountOfPeopleRequiredForGroupDiscount = 6
+const val groupDiscount = 0.1
 
 val daysWhereSecondTicketIsAlwaysFree =
     arrayOf(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY)
 
+val MovieTicket.priceWithPremiumTicketFee
+    get() = price + when {
+        !isPremiumTicket -> 0
+        isStudentOrder -> premiumTicketFeeForStudents
+        else -> premiumTicketFee
+    }
+
 fun calculatePriceForTicket(totalAmountOfTickets: Int, ticketIndex: Int, ticket: MovieTicket): Double {
-    var price = ticket.price
+    val isSecondTicket = ticketIndex.isOdd
+    val isSecondTicketFree = ticket.isStudentOrder || ticket.date.dayOfWeek in daysWhereSecondTicketIsAlwaysFree
+    val isLargeGroup = totalAmountOfTickets >= minimumAmountOfPeopleRequiredForGroupDiscount
 
-    // Adds premium ticket fee for premium tickets
-    if(ticket.isPremiumTicket)
-        price += if(ticket.isStudentOrder) premiumTicketFeeForStudents else premiumTicketFee
-
-    // Adds 10% discount for groups
-    if(!ticket.isStudentOrder && totalAmountOfTickets >= minimumAmountOfPeopleRequiredForGroupDiscount)
-        price -= price * 0.1
-
-    // Makes all even tickets free for students/on weekdays
-    if(ticketIndex % 2 == 1 && (ticket.isStudentOrder || ticket.date.dayOfWeek in daysWhereSecondTicketIsAlwaysFree))
-        price = 0.0
-
-    return price
+    return when {
+        isSecondTicket && isSecondTicketFree -> 0.0
+        !ticket.isStudentOrder && isLargeGroup -> ticket.priceWithPremiumTicketFee * (1.0 - groupDiscount)
+        else -> ticket.priceWithPremiumTicketFee
+    }
 }
