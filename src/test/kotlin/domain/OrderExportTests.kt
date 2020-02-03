677e099b1103866eeddc8a5397b460c4aaed25f2
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-02-03 10:15:25 +0100

    Add 2 OrderExportTests tests

--- src/test/kotlin/domain/OrderExportTests.kt
+++ src/test/kotlin/domain/OrderExportTests.kt
@@ -1,6 +1,7 @@
 package domain
 
 import org.junit.jupiter.api.Test
+import java.time.DayOfWeek
 
 class OrderExportTests {
     @Test
@@ -25,5 +26,45 @@ class OrderExportTests {
 
         // Assert
         assertEquals(actual = text.replace("\n", "").trim(), expected = "[]")
+    }
+
+    @Test
+    fun `Order with one ticket exports plaintext ticket toString() as single line`(){
+        // Arrange
+        val order = Order(1)
+        val ticket = MovieTicket(
+            movieScreening = OrderPriceTests.getScreeningForWeekDay(DayOfWeek.SATURDAY),
+            isStudentOrder = false,
+            isPremiumTicket = false,
+            seatRow = 1,
+            seatNr = 1
+        )
+        order.addSeatReservation(ticket)
+
+        // Act
+        val text = order.getExportedFileContent(TicketExportFormat.PLAINTEXT)
+
+        // Assert
+        assertEquals(actual = text, expected = ticket.toString())
+    }
+
+    @Test
+    fun `Order with one ticket exports json ticket toString() as array with single line`(){
+        // Arrange
+        val order = Order(1)
+        val ticket = MovieTicket(
+            movieScreening = OrderPriceTests.getScreeningForWeekDay(DayOfWeek.SATURDAY),
+            isStudentOrder = false,
+            isPremiumTicket = false,
+            seatRow = 1,
+            seatNr = 1
+        )
+        order.addSeatReservation(ticket)
+
+        // Act
+        val text = order.getExportedFileContent(TicketExportFormat.JSON)
+
+        // Assert
+        assertEquals(actual = text.replace("\n", ""), expected = "[\"$ticket\"]")
     }
 }
